65aae52ab58c1b6d665a82577537941dded42535
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-02-07 10:04:27 -0300

    SafeAreaView, Button, Alert. View

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar, Text, View } from "react-native";
+import {
+  Text,
+  View,
+  StatusBar,
+  SafeAreaView,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const vai = () => {
+    Alert.alert("Titulo da janela", "Fala parça!");
+  };
+
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <View>
-        <Text>Olá React Native!</Text>
-      </View>
+      <SafeAreaView>
+        <View>
+          <Text>Topo/Cabeçalho</Text>
+        </View>
+        <View>
+          <Text>Conteúdo...</Text>
+        </View>
+        <View>
+          <Text>Rodapé...</Text>
+          <Button title="Toca aqui!!!" onPress={vai} />
+        </View>
+      </SafeAreaView>
     </>
   );
 }
