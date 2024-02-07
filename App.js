faec9aede56ec2c0e66e0f0a6695ac91d9783514
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-02-07 11:45:39 -0300

    Estilos para texto

--- App.js
+++ App.js
@@ -19,10 +19,28 @@ export default function App() {
       <StatusBar barStyle="light-content" />
       <SafeAreaView style={estilos.container}>
         <View style={estilos.cabecalho}>
-          <Text>Topo/Cabeçalho</Text>
+          <Text style={estilos.titulo}>Topo/Cabeçalho</Text>
         </View>
         <View style={estilos.conteudo}>
-          <Text>Conteúdo...</Text>
+          <Text style={estilos.subtitulo}>Conteúdo...</Text>
+          <Text style={estilos.texto}>
+            Etiam sed condimentum lorem. Integer sit amet risus dignissim diam
+            tristique suscipit. Cras at erat sed diam maximus dapibus.
+          </Text>
+          <Text style={estilos.texto}>
+            Curabitur scelerisque ipsum non tristique eleifend. Vestibulum
+            tempor leo ut eros rutrum finibus. Praesent ultrices commodo eros,
+            porttitor pellentesque turpis maximus fermentum. Integer a risus et
+            orci vulputate mattis. Pellentesque nec sem cursus, mattis risus
+            non, hendrerit ex.
+          </Text>
+          <Text style={estilos.texto}>
+            Praesent in dapibus diam, nec consectetur nisl. Fusce hendrerit nunc
+            congue, vulputate lacus nec, suscipit lectus. Vestibulum venenatis
+            varius condimentum. Praesent scelerisque ex iaculis justo sodales
+            rhoncus. Curabitur at efficitur ligula, pharetra commodo nibh. Sed
+            eget dignissim ex, sed accumsan turpis.
+          </Text>
         </View>
         <View style={estilos.rodape}>
           <Text>Rodapé...</Text>
@@ -43,6 +61,7 @@ const estilos = StyleSheet.create({
   cabecalho: {
     backgroundColor: "lightgreen",
     flex: 0.6,
+    justifyContent: "center",
   },
   conteudo: {
     backgroundColor: "yellow",
@@ -57,5 +76,18 @@ const estilos = StyleSheet.create({
   rodape: {
     backgroundColor: "orange",
     flex: 0.7,
+  },
+  titulo: {
+    fontSize: 32,
+    textAlign: "center",
+    color: "green",
+    fontWeight: "bold",
+  },
+  subtitulo: {
+    textAlign: "center",
+    fontSize: 24,
+  },
+  texto: {
+    marginTop: 16,
   },
 });
